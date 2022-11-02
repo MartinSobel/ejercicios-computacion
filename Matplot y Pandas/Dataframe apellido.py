4f8aaf1d9be47382698361b71c4486c8b70481f2
Martin <marto.sobel@gmail.com>
2022-11-02 16:56:32 -0300

    matplot y pandas

---
+++ Matplot y Pandas/Dataframe apellido.py
@@ -0,0 +1,4 @@
+# Leer el archivo materias.csv y cargarlo en un dataframe.
+# Graficar Por Apellido en el mismo gráfico, acomodandolos 2 por  fila, el promedio de notas de Matemáticas en amarillo, resaltando los puntos con triángulo hacia abajo y línea continua, 
+#  y el de Geografía en rojo, con línea y punto. Coloque la leyenda. Muestrelo
+
